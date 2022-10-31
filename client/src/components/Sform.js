3ce5b23a200425903f2ec6e871aa596f9e8c2a4a
ali mekni <alimekni5@gmail.com>
2022-10-31 21:22:52 +0100

    modified-Sform

--- client/src/components/Sform.js
+++ client/src/components/Sform.js
@@ -4,13 +4,39 @@ import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
 import jwt_decode from "jwt-decode";
 import userContext from '../contexts/UserContext';
+var  reqotoken;
+const data1={username:"RouaApi",password:"6ec1439555/*"};
+async function fetchData() {
+  try {
+    const myData=  await fetch("https://api.themoviedb.org/3/authentication/token/new?api_key=b3ec5aad46e51258856256128c47b00c");
+   const tokenObj=await myData.json();
+   const token=tokenObj.request_token;
+   const myData1=await fetch("https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=b3ec5aad46e51258856256128c47b00c", {
+    method:'POST',
+    headers:{'Content-Type': 'application/json'},
+    body: JSON.stringify({...data1,request_token:token})
+   });
+   const res=await myData1.json();
+   const myData2=await fetch("https://api.themoviedb.org/3/authentication/session/new?api_key=b3ec5aad46e51258856256128c47b00c", {
+    method:'POST',
+    headers:{'Content-Type': 'application/json'},
+    body: JSON.stringify({request_token:token})
+  });
+  const sessionObj=await myData2.json();
+  const sessionId=sessionObj.session_id;
+  localStorage.setItem("sessionid",sessionId);
+  }catch(reason) {
+    console.log(reason)
+  }finally{
+    console.log("Saye ya rou 5edmett !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  }
+    
+}
 function Sform() {
-    const [password,setpassword]=useState('');
+        const [password,setpassword]=useState('');
     const [email,setemail]=useState('');
     const navigate=useNavigate();
     const data={password:password,email:email};
-    const data1={username:"RouaApi",password:"6ec1439555/*",request_token:localStorage.getItem("sessionToken")};
-    const requesttoken={request_token:localStorage.getItem("sessionToken")};
     const {user,setUser}=useContext(userContext);
     const HandleClick =()=>{
         fetch('http://localhost:4000/login', {
@@ -31,10 +57,7 @@ function Sform() {
                 var decoded = jwt_decode(token);
                 const {username}=decoded;
                 setUser(username);
-                fetch("https://api.themoviedb.org/3/authentication/guest_session/new?api_key=b3ec5aad46e51258856256128c47b00c")
-                .then(response=>response.json())
-                .then(data=>localStorage.setItem("sessionToken",data.guest_session_id))
-                .catch(err=>console.log(err));
+                fetchData();
           }})
             .catch((error) => {
               alert(`Error:${error}`);
