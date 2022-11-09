e60c37f51df38a21ee33226f8654480b18da4325
rouaamdouni <amdouniroua@isamm.u-manouba.tn>
2022-11-09 11:09:30 +0100

    countdwon fixed

--- client/src/components/watchTogether/Countdown.jsx
+++ client/src/components/watchTogether/Countdown.jsx
@@ -1,69 +1,71 @@
-import React from "react";
-const { useState, useEffect } = React;
+import { useEffect, useState } from "react";
+import dahmer from '../../img/dahmer.mp4'
 
-const Countdown = () => {
-  const time=parseInt(localStorage.getItem("time"));
-  console.log(time)
-  const [countdownDate, setCountdownDate] = useState(time);
-  const [state, setState] = useState({
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  });
-  useEffect(() => {
-    setInterval(() => setNewTime(), 1000);
-  }, []);
-  
+function Countdown(){
 
-  const setNewTime = () => {
-    if (countdownDate) {
+
+   const [expiryTime, setExpiryTime] = useState(localStorage.getItem('time'));
+   const [countdownTime, setCountdownTime]= useState(
+       {
+           countdownDays:'',
+           countdownHours:'',
+           countdownlMinutes:'',
+           countdownSeconds:''
+       }
+   );
+
+    const countdownTimer=()=>{
+    
+        const timeInterval= setInterval(() => {
+
+          const countdownDateTime = new Date(expiryTime).getTime(); 
+          const currentTime = new Date().getTime();
+          const remainingDayTime = countdownDateTime - currentTime;
+          const totalDays = Math.floor(remainingDayTime / (1000 * 60 * 60 * 24));
+          const totalHours = Math.floor((remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const totalMinutes = Math.floor((remainingDayTime % (1000 * 60 * 60)) / (1000 * 60));
+          const totalSeconds = Math.floor((remainingDayTime % (1000 * 60)) / 1000);
      
-      const currentTime = new Date().getTime();
-      const distanceToDate = countdownDate - currentTime;
-      let hours = Math.floor(
-        (distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      let minutes = Math.floor(
-        (distanceToDate % (1000 * 60 * 60)) / (1000 * 60)
-      );
-      let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000);
-
-      const numbersToAddZeroTo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-      if (numbersToAddZeroTo.includes(hours)) {
-        hours = `0${hours}`;
-      } else if (numbersToAddZeroTo.includes(minutes)) {
-        minutes = `0${minutes}`;
-      } else if (numbersToAddZeroTo.includes(seconds)) {
-        seconds = `0${seconds}`;
-      }
-      setState({ hours: hours, minutes: minutes, seconds: seconds });
+          const runningCountdownTime={
+             countdownDays: totalDays,
+             countdownHours: totalHours,
+             countdownMinutes: totalMinutes,
+             countdownSeconds: totalSeconds
+          }
+       
+          setCountdownTime(runningCountdownTime);
+     
+          if (remainingDayTime < 0) {
+             clearInterval(timeInterval);
+             setExpiryTime(false);
+            }
+     
+         }, 1000);
     }
-  };
+     
+    useEffect(() => {
+        countdownTimer();
+    });
+   
+    return(
+        <div className="row">
+            <div className="col-sm-6">
+            <div className="btn-group my-3">
+            {expiryTime!==false?
+                <>
+                <button type="button" className="btn btn-outline-success">{countdownTime.countdownDays} <sub>Days</sub></button>
+                <button type="button" className="btn btn-success">:</button>
+                <button type="button" className="btn btn-outline-success">{countdownTime.countdownHours} <sub>Hours</sub></button>
+                <button type="button" className="btn btn-success">:</button>
+                <button type="button" className="btn btn-outline-success">{countdownTime.countdownMinutes} <sub>Minutes</sub></button>
+                <button type="button" className="btn btn-success">:</button>
+                <button type="button" className="btn btn-outline-success">{countdownTime.countdownSeconds} <sub>Seconds</sub></button>
+                </>
+                :<video src={dahmer} autoPlay loop muted />}
+         </div>
+            </div>
+        </div>
+    )
 
-  return (
-    <div>
-      <div className="countdown-wrapper">
-        <div className="time-section">
-          <div className="time">{state.hours || "00"}</div>
-          <small className="time-text">Hours</small>
-        </div>
-        <div className="time-section">
-          <div className="time">:</div>
-        </div>
-        <div className="time-section">
-          <div className="time">{state.minutes || "00"}</div>
-          <small className="time-text">Minutes</small>
-        </div>
-        <div className="time-section">
-          <div className="time">:</div>
-        </div>
-        <div className="time-section">
-          <div className="time">{state.seconds || "00"}</div>
-          <small className="time-text">Seconds</small>
-        </div>
-      </div>
-    </div>
-  );
-};
+}
 export default Countdown;
--- client/src/pages/WatchTogether.jsx
+++ client/src/pages/WatchTogether.jsx
@@ -3,7 +3,6 @@ import io from "socket.io-client";
 import userContext from "../contexts/UserContext";
 import { Chat } from "../components/watchTogether/index"
 import { Navbar } from '../components/global components/index'
-import dahmer from '../img/dahmer.mp4'
 import { Countdown } from "../components/watchTogether/index";
 import { useState,useEffect } from "react";
 export default function WatchTogether() {
@@ -14,10 +13,11 @@ export default function WatchTogether() {
     const seconds = Time.getSeconds();
     const minutes = Time.getMinutes();
     let hours = Time.getHours();
-    const addminutes=minutes +3;
+    const addminutes=minutes +1;
     const year=Time.getFullYear();
     const month=Time.getMonth() ;
     const day=Time.getDate();
+
     let date7= {
         hours:hours,
         minutes:addminutes,
@@ -26,11 +26,8 @@ export default function WatchTogether() {
         month:month,
         day:day,
     }
-    const terfess=new Date(`${date7.year}`,`${date7.month}`,`${date7.day}`,`${date7.hours }`,`${date7.minutes}`,`${date7.seconds}`);
-    console.log(terfess)
-    const timee=terfess.getTime();
-    console.log(timee)
-    localStorage.setItem("time",timee);
+    const terfess=`${day} ${'nov'} ${year} ${hours}:${addminutes}:${seconds} `
+    localStorage.setItem("time",terfess);
   }
    useEffect(()=> {
     getTime();
@@ -48,10 +45,9 @@ export default function WatchTogether() {
       {joinRoom()}
       <Navbar />
       <div style={{display:'flex', justifyContent:'space-between'}}>       
-      <video src={dahmer} autoPlay loop muted />
       <Chat socket={socket} username={user} room={localStorage.getItem('roomid')} />
       </div>
-      <Countdown time={date}/>
+      <Countdown />
     </div>
   );
 
